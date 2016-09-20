f6f0f299937654869e6693ec100d52c20502b388
jagh <jagh1729@gmail.com>
2016-09-20 15:14:22 +0200

    Recording data while the application is running for desktop machine

--- dataCapture/enerGyPU_record.sh
+++ dataCapture/enerGyPU_record.sh
@@ -12,7 +12,7 @@
 ###########################################################################
 
 
-# execution in background without enerGyPU_run.sh
+# Execution in background without enerGyPU_run.sh
 Dir=../testbed/
 HOST=$(hostname)
 DATA=`date +%Y%m%d%H%M`
@@ -21,4 +21,24 @@ mkdir $Dir/$HOST-$DATA-$APP
 
 # Identification of the GPU in the machine
 i=0; for id in $(nvidia-smi | grep 0000 | awk '{print $8}'); do GPU[$i]=$id; i=$i+1; done; 
-echo ${GPU[*]};
+echo ${#GPU[@]} : ${GPU[*]};
+
+# Recording data while the application is running
+while true
+do
+
+Time=`date +%s`
+nvidia-smi -q -i ${GPU[*]} -d MEMORY,UTILIZATION,TEMPERATURE,POWER,CLOCK |
+grep -e "Timestamp" -e "MiB" -e "W" -e "MHz" |
+awk '{if(NR == 1){TIMENV=$6; print "GPU00: " TIMENV "-" '$Time';}
+      if($1 == "Used" && NR==3) USED=$3; 
+ else if($1 == "Free" && NR==4) FREE=$3;
+ else if($2 == "Draw" && NR==8) DRAW=$4;
+ else if($1 == "Graphics" && NR==9) GRAPHICS=$3;
+ else if($1 == "SM"&& NR==18) SM=$3;
+ else if($1 == "Memory"&& NR==19) MEMORY=$3;
+ else if(NR%26 == 0)
+ print TIMENV";"GRAPHICS";"SM";"MEMORY";"USED";"FREE";"DRAW >> "'$Dir/$HOST-$DATA-$APP/$HOST-$DATA-$APP-'gpu'${GPU[@]}'.csv"}'
+
+sleep 0.9s
+done
