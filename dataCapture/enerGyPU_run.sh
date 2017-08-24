322dd9096c4b45615a2feee7fea57d3a7e5b8588
jagh <jagh1729@gmail.com>
2017-08-24 16:40:18 +0200

    Identification of the GPU in the machine

--- dataCapture/enerGyPU_run.sh
+++ dataCapture/enerGyPU_run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-###########################################################################
-#                                                                         #
-# enerGyPU for monitoring performance and power consumption on Multi-GPU  #
-#                                                                         #
-###########################################################################
-
-# enerGyPU_run.sh
-# Execution steps and the application libraries are declared.
-###########################################################################
+#############################################################################
+#                                                                         	#
+# enerGyPU for monitoring performance and power consumption on Multi-GPU  	#
+#                                                                         	#
+#############################################################################
+# enerGyPU_run.sh															#
+# Execution steps and the application libraries are declared.				#
+#############################################################################
 
 
 # Global workload parameters
@@ -27,25 +26,24 @@ mkdir $Dir/$ARGV
 
 ## Identification of the GPU in the machine
 i=0; for id in $(nvidia-smi | grep 0000 | awk '{print $8}'); do GPU[$i]=$id; i=$i+1; done;
-#echo ${#GPU[@]} : ${GPU[*]};
-
+echo ${#GPU[@]} : ${GPU[*]};
 if [ ${#GPU[@]} == 1 ]
-then
-	
+then	
 	## Executes the enerGyPU_record.sh
-	./enerGyPU_record-desktop.sh $Dir $ARGV &
-elif [ ${#GPU[@]} == 1 ]
+	./enerGyPU_record-desktop.sh $Dir $ARGV #&
+
+elif [ ${#GPU[@]} == 8 ]
 then
 	## Executes the enerGyPU_record.sh
     ./enerGyPU_record-cluster.sh $Dir $ARGV &
+
 else
 	echo "++ no NVIDIA GPU detected ++"
 fi
 
 
-
 ## Add path of the application and libraries necessaries
-nvprof --print-gpu-trace -o $Dir/$ARGV/$ARGV-matrixMul.nvprof ./../samples/matrixMul/matrixMul -device=$nGPU -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
+#nvprof --print-gpu-trace -o $Dir/$ARGV/$ARGV-matrixMul.nvprof ./../samples/matrixMul/matrixMul -device=$nGPU -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
 
 
 kill %1
