31b718e9c6d18d01d1e29abcf6db209e0393bfa7
jagh <jagh1729@gmail.com>
2017-08-24 14:57:40 +0200

    Identification of the GPU in the machine

---
+++ dataCapture/enerGyPU_record-desktop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+###########################################################################
+#									  #
+# enerGyPU for monitoring performance and power consumption on Multi-GPU  #
+#		                                                          #
+###########################################################################				
+
+# enerGyPU_record.sh
+# Data extration and write one separate file for each GPU on "testbed":
+# Power consumption, streeming multiprocessor clock and memory clock frequency.
+###########################################################################
+
+## Execution with enerGyPU_run.sh
+Dir=$1
+ARGV=$2
+
+## Execution in background without enerGyPU_run.sh
+#Dir=../testbed/
+#HOST=$(hostname)
+#APP="matrixMul"
+#DATA=`date +%Y%m%d%H%M`
+#ARGV=$HOST-$APP-$DATA
+#mkdir $Dir/$ARGV
+
+## Identification of the GPU in the machine
+i=0; for id in $(nvidia-smi | grep 0000 | awk '{print $8}'); do GPU[$i]=$id; i=$i+1; done; 
+echo ${#GPU[@]} : ${GPU[*]};
+
+## Recording data while the application is running
+while true
+do
+
+Time=`date +%s`
+nvidia-smi -q -i ${GPU[*]} -d MEMORY,UTILIZATION,TEMPERATURE,POWER,CLOCK |
+grep -e "Timestamp" -e "MiB" -e "W" -e "MHz" |
+awk '{if(NR == 1){TIMENV=$6; print "GPU00: " TIMENV "-" '$Time';}
+      if($1 == "Used" && NR==3) USED=$3; 
+ else if($1 == "Free" && NR==4) FREE=$3;
+ else if($2 == "Draw" && NR==8) DRAW=$4;
+ else if($1 == "Graphics" && NR==9) GRAPHICS=$3;
+ else if($1 == "SM"&& NR==18) SM=$3;
+ else if($1 == "Memory"&& NR==19) MEMORY=$3;
+ else if(NR%26 == 0)
+ print TIMENV";"'$Time'";"GRAPHICS";"SM";"MEMORY";"USED";"FREE";"DRAW >> "'$Dir/$ARGV/$ARGV-'gpu'${GPU[@]}'.csv"}'
+
+sleep 0.9s
+done
---
+++ dataCapture/enerGyPU_run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+###########################################################################
+#                                                                         #
+# enerGyPU for monitoring performance and power consumption on Multi-GPU  #
+#                                                                         #
+###########################################################################
+
+# enerGyPU_run.sh
+# Execution steps and the application libraries are declared.
+###########################################################################
+
+
+# Global workload parameters
+# For the sample matrixMul the dimensions of A & B matrices must be equal.
+DIM=1024
+nGPU=1
+
+# Location of the power consumption measures
+Dir=../testbed/
+HOST=$(hostname)
+APP="matrixMul"
+DATA=`date +%Y%m%d%H%M`
+ARGV=$HOST-$APP-$DIM-$nGPU-$DATA
+mkdir $Dir/$ARGV
+
+
+## Identification of the GPU in the machine
+i=0; for id in $(nvidia-smi | grep 0000 | awk '{print $8}'); do GPU[$i]=$id; i=$i+1; done;
+#echo ${#GPU[@]} : ${GPU[*]};
+
+if [ ${#GPU[@]} == 1 ]
+then
+	
+	## Executes the enerGyPU_record.sh
+	./enerGyPU_record-desktop.sh $Dir $ARGV &
+elif [ ${#GPU[@]} == 1 ]
+then
+	## Executes the enerGyPU_record.sh
+    ./enerGyPU_record-cluster.sh $Dir $ARGV &
+else
+	echo "++ no NVIDIA GPU detected ++"
+fi
+
+
+
+## Add path of the application and libraries necessaries
+nvprof --print-gpu-trace -o $Dir/$ARGV/$ARGV-matrixMul.nvprof ./../samples/matrixMul/matrixMul -device=$nGPU -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
+
+
+kill %1
