20bd6fdf66ccf292c7a7f63c31242cce7df00eee
jagh <jagh1729@gmail.com>
2018-09-05 08:31:24 +0000

    Added bandwidth monitor to record between the master and each worker

---
+++ dataCapture/enerGyPU_bandwidth.sh
@@ -0,0 +1,52 @@
+
+###########################################################################
+#                                                                         #
+# enerGyPU for monitoring performance and power consumption on Multi-GPU  #
+#                                                                         #
+###########################################################################
+
+# enerGyPU_record for Jetson TX2
+# Data extration and write one separate file for each GPU on "testbed":
+# Power consumption, streeming multiprocessor clock and memory clock frequency.
+#####################################################################
+
+
+## Execution with enerGyPU_run.sh
+Dir=$1
+ARGV=$2
+TFPS=$3	## TensorFlow parameter server
+HOST=$(hostname)
+
+## Execution in background without enerGyPU_run.sh
+#Dir=../testbed/
+#HOST=$(hostname)
+#APP="matrixMul"
+#DATA=`date +%Y%m%d%H%M`
+#ARGV=$HOST-$APP-$DATA
+#mkdir $Dir/$ARGV
+## TensorFlow parameter server
+#TFPS="134.59.132.20:2222"
+
+## Get host from TensorFlow parameter server 
+FLAG=0
+for addr in $(echo $TFPS | tr ":" "\n")
+do
+   if [ $FLAG == 0 ]; then
+      MASTER=$addr
+      FLAG=1
+   fi
+done
+
+## Recording data while the application is running
+while true
+do
+
+Time=`date +%s`
+
+#sudo ./tegrastats |
+#sudo iftop -t -f "host $MASTER" | grep $HOST".unice.fr" |
+sudo iftop -t -f "host $MASTER" | grep -e "Total" | awk '{print '$Time'";;"$0 $1 $2 $3 >> "'$Dir/$ARGV/$ARGV'-bandwidth.txt"}'
+
+
+#sleep 0.9s
+done
