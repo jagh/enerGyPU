a0e72a6c914d6f42cd3da593f55f2935243019c4
jagh <jagh1729@gmail.com>
2018-10-23 09:55:35 +0200

    Updated GPU traces using the new nvidia-smi Queries

--- dataCapture/enerGyPU_run.sh
+++ dataCapture/enerGyPU_run.sh
@@ -23,31 +23,11 @@ DATA=`date +%Y%m%d%H%M`
 ARGV=$HOST-$APP-$DIM-$nGPU-$DATA
 mkdir $Dir/$ARGV
 
-###############################################
-## OLD-Version
-## Identification of the GPU in the machine
-#i=0; for id in $(nvidia-smi | grep 0000 | awk '{print $8}'); do GPU[$i]=$id; i=$i+1; done;
-#echo ${#GPU[@]} : ${GPU[*]};
-#if [ ${#GPU[@]} == 1 ]
-#then	
-#	## Executes the enerGyPU_record.sh
-#	./enerGyPU_record-desktop.sh $Dir $ARGV &
-#
-#elif [ ${#GPU[@]} == 8 ]
-#then
-#	## Executes the enerGyPU_record.sh
-#    ./enerGyPU_record-cluster.sh $Dir $ARGV &
-#
-#else
-#	echo "++ no NVIDIA GPU detected ++"
-#fi
-###############################################
-
 ## Executes the enerGyPU_record.sh
 ./enerGyPU_record.sh $Dir $ARGV &
 
 ## Add path of the application and libraries necessaries
 nvprof --print-gpu-trace -o $Dir/$ARGV/$ARGV-matrixMul.nvprof ./../samples/matrixMul/matrixMul -device=$nGPU -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
 
-
 kill %1
+killall -9 nvidia-smi
