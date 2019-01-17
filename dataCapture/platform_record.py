5537f687d2d9d362213616d9fe5057c59812ebe1
jagh <jagh1729@gmail.com>
2019-01-17 12:57:15 +0100

    Added Monitor for recording memory, cpu and disk usage while the models are training

---
+++ dataCapture/platform_record.py
@@ -0,0 +1,44 @@
+"""
+Monitor for recording memory, cpu and disk usage while the models are training
+"""
+
+import sys
+import psutil
+import os, datetime
+import numpy as np
+
+
+def main():
+    if len(sys.argv) != 4:
+        sys.exit('usage: platform <pid> <testbed_exp> <exp_id>')
+        
+    p = psutil.Process(int(sys.argv[1]))
+    metrics_fname = str(sys.argv[2]+"/"+sys.argv[3]+'-resources_metrics.txt')
+
+    with open(metrics_fname, 'w+') as f:
+        while True:
+            core_usage = str(p.cpu_percent(interval=1.0))
+            num_threads = str(p.num_threads())
+            memory_usage = str(np.round(p.memory_percent(), 2))
+            memory_rss = str(p.memory_info().rss / 1024)
+            memory_vms = str(p.memory_info().vms / 1024)
+            memory_shr = str(p.memory_info().shared / 1024)
+            data = str(p.memory_info().data)
+            io_reads = str(p.io_counters().read_bytes / 1024)
+            io_writes = str(p.io_counters().write_bytes / 1024)
+            time = str(datetime.datetime.now().time())
+
+
+
+            resources_metrics = str(time+","+memory_usage+","+memory_rss+","+memory_vms+","+memory_shr+","+
+                                    core_usage+","+num_threads+","+data+","+io_reads+","+io_writes)
+            # 
+            # resources_metrics = str(np.hstack((time, memory_rss, memory_rss,
+            #                             memory_vms, memory_shr, core_usage,
+            #                             num_threads, data, io_reads, io_writes)))
+            # print(resources_metrics)
+            f.write(resources_metrics + '\n')
+        f.close()
+        
+if __name__ == '__main__':
+    main()
