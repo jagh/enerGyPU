fc09fd630749602c63a4892034bc9cf2ad3e07ff
jagh <jagh1729@gmail.com>
2019-06-19 09:42:24 +0000

    Modified bandwidth monitor to support mpi distributed training

--- dataCapture/enerGyPU_bandwidth.sh
+++ dataCapture/enerGyPU_bandwidth.sh
@@ -14,7 +14,8 @@
 ## Execution with enerGyPU_run.sh
 Dir=$1
 ARGV=$2
-TFPS=$3	## TensorFlow parameter server
+#TFPS=$3	## TensorFlow parameter server
+MASTER=$3
 HOST=$(hostname)
 
 ## Execution in background without enerGyPU_run.sh
@@ -22,29 +23,31 @@ HOST=$(hostname)
 #HOST=$(hostname)
 #APP="matrixMul"
 #DATA=`date +%Y%m%d%H%M`
-#ARGV=$HOST-$APP-$DATA
+#ARGV=$APP-$DATA
 #mkdir $Dir/$ARGV
 ## TensorFlow parameter server
 #TFPS="134.59.132.20:2222"
+#MASTER=134.59.132.111
 
 ## Get host from TensorFlow parameter server 
-FLAG=0
-for addr in $(echo $TFPS | tr ":" "\n")
-do
-   if [ $FLAG == 0 ]; then
-      MASTER=$addr
-      FLAG=1
-   fi
-done
+#FLAG=0
+#for addr in $(echo $TFPS | tr ":" "\n")
+#do
+#   if [ $FLAG == 0 ]; then
+#      MASTER=$addr
+#      FLAG=1
+#   fi
+#done
 
 ## Recording data while the application is running
-while true
-do
+#while true; do
+#Time=`date +%s`
+#sudo iftop -t -f "host $MASTER" | grep -e "Total" -e "Cumulative" | awk '{print '$Time'";;"$0 >> "'$Dir/$ARGV/$HOST-$ARGV'-bandwidth.txt"}'
 
-Time=`date +%s`
 
-sudo iftop -t -f "host $MASTER" | grep -e "Total" -e "Cumulative" | awk '{print '$Time'";;"$0 >> "'$Dir/$ARGV/$ARGV'-bandwidth.txt"}'
+sudo iftop -t -f "host $MASTER" | grep -e "Total" -e "Cumulative" |
+ awk '{print $0 >> "'$Dir/$ARGV/$HOST-$ARGV'-bandwidth.txt"}'
 
 
 #sleep 0.9s
-done
+#done
